57c400d8255faa2da15588bad0ae7b73ff3779a2
Clemence PAVY <clemence.pavy@etu.unicaen.fr>
2024-11-04 11:07:40 +0100

    correction mvc

--- index.php
+++ index.php
@@ -1,60 +1,82 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
 
-    require 'autoload.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    class Router
+require 'autoload.php';
+
+class Router
+{
+    private $routes = [];
+    private $prefix;
+
+    public function __construct($prefix = '')
     {
-        private $routes = [];
-        private $prefix;
-
-        public function __construct($prefix = '')
-        {
-            $this->prefix = trim($prefix, '/');
-        }
-
-        public function addRoute($uri, $controllerMethod)
-        {
-            $this->routes[trim($uri, '/')] = $controllerMethod;
-        }
-
-        public function route($url)
-        {
-            // Suppression du préfixe du début de l'URL
-            if ($this->prefix && strpos($url, $this->prefix) === 0) {
-                $url = substr($url, strlen($this->prefix) + 1);
-            }
-
-            // Suppression des barres obliques en trop
-            $url = trim($url, '/');
-
-            // Vérification de la correspondance de l'URL à une route définie
-            if (array_key_exists($url, $this->routes)) {
-                // Extraction du nom du contrôleur et de la méthode
-                list($controllerName, $methodName) = explode('@', $this->routes[$url]);
-
-                // Instanciation du contrôleur et appel de la méthode
-                $controller = new $controllerName();
-                $controller->$methodName();
-
-                
-            } else {
-                // Gestion des erreurs (page 404, etc.)
-                require_once 'views/404.php';
-            }
-        }
+        $this->prefix = trim($prefix, '/');
     }
 
-    // Instanciation du routeur
-    $router = new Router('MVC'); //marche seulement en local (avec xampp)
+    public function addRoute($uri, $controllerMethod)
+    {
+        $this->routes[trim($uri, '/')] = $controllerMethod;
+    }
 
-    // Ajout des routes
-    $router->addRoute('', 'HomeController@index'); // Pour la racine
-    $router->addRoute('tasks', 'TaskController@index'); // Pour la racine
-    $router->addRoute('about', 'HomeController@about'); // Pour la racine
+    public function route($url)
+    {
+        // Enlève le préfixe du début de l'URL
+        if ($this->prefix && strpos($url, $this->prefix) === 0) {
+            $url = substr($url, strlen($this->prefix) + 1);
+        }
 
-    // Appel de la méthode route
-    $router->route(trim($_SERVER['REQUEST_URI'], '/'));
-?>
+        // Enlève les barres obliques en trop
+        $url = trim($url, '/');
+
+        // Vérification de la correspondance de l'URL à une route définie
+        foreach ($this->routes as $route => $controllerMethod) {
+            // Vérifie si l'URL correspond à une route avec des paramètres
+            $routeParts = explode('/', $route);
+            $urlParts = explode('/', $url);
+
+            // Si le nombre de segments correspond
+            if (count($routeParts) === count($urlParts)) {
+                // Vérification de chaque segment
+                $params = [];
+                $isMatch = true;
+                foreach ($routeParts as $index => $part) {
+                    if (preg_match('/^{\w+}$/', $part)) {
+                        // Capture les paramètres
+                        $params[] = $urlParts[$index];
+                    } elseif ($part !== $urlParts[$index]) {
+                        $isMatch = false;
+                        break;
+                    }
+                }
+
+                if ($isMatch) {
+                    // Extraction du nom du contrôleur et de la méthode
+                    list($controllerName, $methodName) = explode('@', $controllerMethod);
+
+                    // Instanciation du contrôleur et appel de la méthode avec les paramètres
+                    $controller = new $controllerName();
+                    call_user_func_array([$controller, $methodName], $params);
+                    return;
+                }
+            }
+        }
+
+        // Si aucune route n'a été trouvée, gérer l'erreur 404
+        require_once 'views/404.php';
+    }
+}
+
+// Instanciation du routeur
+$router = new Router('MVC');
+
+// Ajout des routes
+$router->addRoute('', 'HomeController@index'); // Pour la racine
+$router->addRoute('tasks', 'TaskController@index'); // Pour la liste des tâches
+$router->addRoute('tasks/{id}', 'TaskController@show'); // Pour afficher une tâche par ID
+$router->addRoute('about', 'HomeController@about'); // Pour la page "about"
+
+// Appel de la méthode route
+$router->route(trim($_SERVER['REQUEST_URI'], '/'));
